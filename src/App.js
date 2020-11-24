dd900e6816c769a405227acbc1e5919b30b3496c
stabassum <sumu1993@gmail.com>
2020-11-23 19:51:11 -0500

    commit tree 3aa0a1baab9192106f6222df77997b1909307005

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import CatComponent from './CatComponent.js'
+// import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
+// import MouseComponent from './MouseComponent.js'
+
+// class App extends Component {
+// 	render() {
+// 		// your code in the return statement below!
+// 		return (
+// 			<div className="App">
+// 				<CatComponent />
+// 				<GraceHopperQuoteComponent />
+// 				<MouseComponent />
+// 			</div>
+// 		);
+// 	}
+// }
+
+// export default App;
+
+import React, {Component} from 'react';
 import CatComponent from './CatComponent.js'
-import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
+import {GraceHopperComponent} from './GraceHopperQuoteComponent.js'
 import MouseComponent from './MouseComponent.js'
 
-class App extends Component {
-	render() {
-		// your code in the return statement below!
-		return (
+class App extends React.Component{
+	render(){
+		return(
 			<div className="App">
 				<CatComponent />
-				<GraceHopperQuoteComponent />
 				<MouseComponent />
+				<GraceHopperComponent />
 			</div>
-		);
+		)
 	}
 }
 
--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export class GraceHopperQuoteComponent extends Component {
+class GraceHopperQuoteComponent extends React.Component {
 	render() {
 		return (
 			<div className="bar" id="hopper">
@@ -15,3 +15,5 @@ export class GraceHopperQuoteComponent extends Component {
 		);
 	}
 }
+
+export default GraceHopperQuoteComponent;
